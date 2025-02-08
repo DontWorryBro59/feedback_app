d2a94463a609ec2a6f6eb44f2e69b2c7533bccbb
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-02-08 14:49:42 +0500

    Создана форма для ввода отзыва пользователем, реализована сессия для пользователя, написана базовая идея для панели администратора (вход и переход на саму панель конфигурации)

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
+CONSULTANTS = ['Виктор Петрухин', 'Дарья Аляпина', 'Татьяна Аляпко']
+#CONSULTANTS = []
 app = Flask(__name__, static_folder='static', static_url_path='/')
-
+app.secret_key = 'My_very_very_secret_key'
 
 from myapp import models
 from myapp import routes
+
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,61 @@
-from flask import render_template
-from myapp import app
+from flask import render_template, request, session, redirect, url_for, flash
+from myapp import app, CONSULTANTS
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        if 'number' in session:
+            return render_template('thanks.html')
+        return render_template('index.html', consultants=CONSULTANTS)
+    else:
+        consult_name = request.form.get('consult_name')
+        date = request.form.get('date')
+        comment = request.form.get('comment')
+        rating = request.form.get('rating')
+        phone_number = request.form.get('number')
+        print(consult_name, date, comment, rating, phone_number)
+        session['number'] = phone_number
+        return render_template('thanks.html')
+
+
+@app.route('/logout/', methods=['GET'])
+def logout():
+    if 'number' in session:
+        session.pop('number', None)
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+
+@app.route('/admin_login/', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'GET':
+        return render_template('admin_login.html')
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'admin' and password == '123pass456!':
+            session['admin'] = 'admin'
+            return redirect(url_for('admin_panel'))
+
+        else:
+            flash('Ошибка ввода логина и пароля', 'errors')
+            return render_template('admin_login.html')
+
+@app.route('/admin_panel/')
+def admin_panel():
+    if 'admin' in session:
+        return render_template('admin_panel.html')
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/admin_logout/')
+def admin_logout():
+    if 'admin' in session:
+        session.pop('admin', None)
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
